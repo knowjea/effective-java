b809ad3f052c3b32f5e85ff45c79ca4e21779965
Arum <knowjea69@gmail.com>
2018-07-01 17:25:36 +0900

    Avoid creating unnecessary objects

---
+++ src/rule5/Rule5.java
@@ -0,0 +1,21 @@
+package rule5;
+
+/**
+ * Avoid creating unnecessary objects
+ * (Do not create unnecessary objects)
+ *
+ * @author gwon
+ * @history
+ *          2018. 7. 01. initial creation
+ */
+public class Rule5 {
+	public static void main(String[] args) {
+//		String str = new String("stringette");
+		String str = "stringette";
+
+//		Boolean bool = new Boolean(true);
+		Boolean bool = Boolean.valueOf(true);
+
+	}
+
+}
