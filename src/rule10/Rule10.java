1b31a3b9913b0c9c06177e90e4212d4feecfc2ad
Arum <knowjea69@gmail.com>
2018-09-09 15:29:50 +0900

    rule10

---
+++ src/rule10/Rule10.java
@@ -0,0 +1,14 @@
+package rule10;
+
+/**
+ *
+ *
+ * @author gwon
+ * @history
+ *          2018. 9. 2. initial creation
+ */
+public class Rule10 {
+	public static void main(String[] args) {
+		new Object().toString();
+	}
+}
